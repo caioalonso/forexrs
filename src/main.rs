05eef9255c8df9733042bdf4bf470c74c1f4e6ec
Caio Alonso <caio@caioalonso.com>
2018-01-02 23:09:12 -0200

    Parses the CSV file into a struct vector

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,43 @@
-use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
 extern crate time;
 
+#[derive(Debug)]
+struct Tick {
+    time: time::Tm,
+    ask: f32,
+    bid: f32,
+    ask_volume: u32,
+    bid_volume: u32,
+}
+
 fn main() {
-    let path = Path::new("/home/caio/dev/forex/tick/short.csv");
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
-        Ok(file) => file,
-    };
-
+    let mut file =
+        File::open("/home/caio/dev/forex/tick/short.csv").expect("Failed to open CSV file.");
     let mut s = String::new();
     file.read_to_string(&mut s).expect("Couldn't read file");
-    for line in s.split('\n') {
-        let items: Vec<&str> = line.split(',').collect();
-        match time::strptime(items[0], "%Y-%m-%d %H:%M:%S.%f")
-            {
-                Ok(v) => println!("{}", time::strftime("%Y/%m/%d %H:%M:%S.%f",
-                                                       &v).unwrap()),
-                Err(e) => println!("Error: {}", e),
-            };
+    let ticks: Vec<Tick> = s.lines().filter_map(parse_line).collect();
+    println!("{:#?}", ticks);
+}
+
+fn parse_line(line: &str) -> Option<Tick> {
+    let items: Vec<&str> = line.split(',').collect();
+    let time = time::strptime(items[0], "%Y-%m-%d %H:%M:%S.%f");
+    match time {
+        Ok(t) => {
+            let time = t;
+            let ask = items[1].parse::<f32>().unwrap();
+            let bid = items[2].parse::<f32>().unwrap();
+            let ask_volume = items[3].parse::<u32>().unwrap();
+            let bid_volume = items[4].parse::<u32>().unwrap();
+            Some(Tick {
+                time,
+                ask,
+                bid,
+                ask_volume,
+                bid_volume,
+            })
+        }
+        Err(_) => None,
     }
 }
