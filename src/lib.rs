4a72fbe75bf207d8c3c92a512e8c6fdb89a1a942
Caio Alonso <caio@caioalonso.com>
2018-01-10 23:10:39 -0200

    Basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -70,11 +70,7 @@ impl Candle {
 
 pub fn run(csv: &str) {
     let ticks = parse_csv(csv);
-    let duration = time::Duration::minutes(1);
-    let mut candles: Vec<Candle> = Vec::new();
-    for tick in ticks {
-        candles = update_candles(candles, tick, duration);
-    }
+    build_candles(ticks);
 }
 
 fn parse_csv(filestr: &str) -> Vec<Tick> {
@@ -84,6 +80,15 @@ fn parse_csv(filestr: &str) -> Vec<Tick> {
         .read_to_string(&mut s)
         .expect("Couldn't read file");
     s.lines().filter_map(Tick::new).collect()
+}
+
+fn build_candles(ticks: Vec<Tick>) -> Vec<Candle> {
+    let duration = time::Duration::minutes(1);
+    let mut candles: Vec<Candle> = Vec::new();
+    for tick in ticks {
+        candles = update_candles(candles, tick, duration);
+    }
+    candles
 }
 
 fn update_candles(mut candles: Vec<Candle>, tick: Tick, duration: time::Duration) -> Vec<Candle> {
@@ -105,7 +110,38 @@ fn update_candles(mut candles: Vec<Candle>, tick: Tick, duration: time::Duration
     candles
 }
 
-#[test]
-fn test_something() {
-    panic!("sei la");
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parses_valid_csv() {
+        let ticks = parse_csv("oneday.csv");
+        assert_eq!(build_candles(ticks).len(), 1440);
+    }
+
+    #[test]
+    #[should_panic]
+    fn doesnt_parse_nonexistant_file() {
+        println!("{:?}", run("nonexistant.csv"));
+    }
+
+    #[test]
+    fn updates_price() {
+        let first_tick = Tick {
+            time: time::now(),
+            ask: 1.0,
+            bid: 1.0,
+            ask_volume: 10,
+            bid_volume: 10
+        };
+        let mut c = Candle::new(first_tick);
+        c.update_price(2.0);
+        c.update_price(0.5);
+        c.update_price(1.5);
+        assert_eq!(c.o, 1.0);
+        assert_eq!(c.h, 2.0);
+        assert_eq!(c.l, 0.5);
+        assert_eq!(c.c, 1.5);
+    }
 }
